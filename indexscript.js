2d7813b972b8e83ba19c25064289d8a04768bc97
akdon1198 <akdon1198@gmail.com>
2024-07-21 19:19:04 +0530

    responsiveness done

---
+++ indexscript.js
@@ -0,0 +1,14 @@
+let img = document.querySelectorAll(".header>img")[1]
+let link = document.querySelector(".links2")
+console.log(link)
+let toggle = 1
+img.addEventListener("click", function(){
+    if(toggle){
+        link.style.display = "flex",
+        toggle = 0
+    }else{
+        link.style.display = "none",
+        toggle = 1        
+    }
+    console.log(img)
+})
